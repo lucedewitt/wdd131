cc8543d1b119cb146206edc3d7f93a9eb141ee25
lucedewitt <lucedewitt@byui.edu>
2025-05-15 11:05:44 -0600

    events

---
+++ coolpics/coolpics.js
@@ -0,0 +1,10 @@
+const button = document.querySelector(button);
+// closeButton.addEventListener('click', () => {
+//     modal.close();
+// })
+
+// modal.addEventListener('click', (event) => {
+//     if (event.target === modal) {
+//         modal.close();
+//     }
+// })
